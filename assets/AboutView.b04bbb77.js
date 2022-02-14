55e6a63cfc9affe7621aaa8c88bd92432de13c42
Lee Segal <segalee17@gmail.com>
2022-02-14 17:56:20 +0200

    add build

---
+++ assets/AboutView.b04bbb77.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.7ac3ae56.js";import{o,c as t,a as s}from"./vendor.dfab41c5.js";const a={},c={class:"about"},r=s("h1",null,"This is an about page",-1),_=[r];function n(i,d){return o(),t("div",c,_)}var u=e(a,[["render",n]]);export{u as default};
